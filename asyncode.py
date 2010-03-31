8ef02bdf4e7adb6653e96df1c0c76433d21035a4
adrian <adrian@54d9d9e8-c0c9-4766-b84c-5b6bd2188e6d>
2010-03-31 14:04:24 +0000

    heh svn co sometimes is not working

--- asyncode.py
+++ asyncode.py
@@ -9,11 +9,12 @@ from ACF import globals
 import getpass
 username=getpass.getuser()
 print username
-__import__(username+"_conf")
+username=getpass.getuser()
 try:
 	__import__(username+"_conf")
 except:
 	raise Exception("This is development version of ACF! You should download the ACF GA.")
+
 #this is ugly but we need to set the absolute path so whole system knows where get files from
 #TODO export settings to /etc/ACF/config
 #globals.ACFconf="/home/adrian/ACF/ACFconf.xml"
