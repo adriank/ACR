1e1c3f11700986d91ccc866bdfa651fceb26df88
adrian <adrian@54d9d9e8-c0c9-4766-b84c-5b6bd2188e6d>
2010-04-01 12:01:43 +0000

    hot fix

--- asyncode.py
+++ asyncode.py
@@ -15,8 +15,11 @@ try:
 except:
 	raise Exception("This is development version of ACF! You should download the ACF GA.")
 sys.argv[0] = 'acf'
-apps=filter(os.path.isdir, map(lambda f: os.path.join(globals.appsDir,f),os.listdir(globals.appsDir)))
-print apps
+try:
+	apps=filter(os.path.isdir, map(lambda f: os.path.join(globals.appsDir,f),os.listdir(globals.appsDir)))
+	print apps
+except:
+	pass
 #this is ugly but we need to set the absolute path so whole system knows where get files from
 #TODO export settings to /etc/ACF/config
 #globals.ACFconf="/home/adrian/ACF/ACFconf.xml"
