1bfcf5e74920309f429fc8eecf833ab16c50100c
adrian <adrian@54d9d9e8-c0c9-4766-b84c-5b6bd2188e6d>
2010-10-09 20:20:38 +0000

    added datagenerator

---
+++ ACF/components/datagenerator.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# AsynCode Framework - XML framework allowing developing internet
+# applications without using programming languages.
+# Copyright (C) 2008-2010  Adrian Kalbarczyk
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the version 3 of GNU General Public License as published by
+# the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from ACF.components import *
+from ACF.utils import replaceVars
+from ACF.utils.xmlextras import tree2xml
+from ACF import globals
+from ACF.errors import Error
+from ACF.utils import json
+
+class DataGenerator(Component):
+	def list(self,acenv,conf):
+		return List(replaceVars(acenv, conf["content"]))
+
+	def generate(self,acenv,conf):
+		return self.__getattribute__(conf["command"].split(":").pop())(acenv,conf)
+
+	def parseAction(self,config):
+		ret=config["params"].copy()
+		ret["command"]=config["command"]
+		if ret.has_key("input") and ret["input"]=="json":
+			ret["content"]=json.loads("".join(config["content"]))
+		else:
+			r=[]
+			for i in config["content"]:
+				r.append(tree2xml(i))
+			ret["content"]="".join(r)
+		return ret
+
+def getObject(config):
+	return DataGenerator(config)
