548403e722e2ba7229ff444046c42faa88361bdc
Adrian Kalbarczyk <adrian@adrian-ac.(none)>
2011-03-31 14:45:00 +0200

    fixed wsgi handler

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+sys.path.append("/root/acr/ACRA")
+sys.path.append("/root/acr")
 from ACR.backends.WSGIHandler import application
+from ACR import acconfig
+acconfig.appsDir="/var/ACF"
