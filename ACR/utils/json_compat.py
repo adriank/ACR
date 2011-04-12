299bdf35ebd766bbfb416e1c19538dc224ae333a
Adrian Kalbarczyk <adrian@adrian-ac.(none)>
2011-04-12 13:51:24 +0200

    added exception handling in JSON

--- ACR/utils/json_compat.py
+++ ACR/utils/json_compat.py
@@ -2,35 +2,20 @@
 from ACR.errors import Error
 
 try:
-	import cjson3
-	print "cjson"
-	def dumps(s,default):
-		print s
-		return cjson.encode(s)
-	#TODO unify cjson API with simplejson/json
+	import json
 except:
 	try:
-		import json
+		import simplejson as json
 	except:
-		try:
-			import simplejson as json
-		except:
-			raise Error("JSONNotFound")
+		raise Error("JSONNotFound")
 
-#def loads(s,object_hook=None):
-	#def tostr(dct):
-	#	d={}
-	#	for key in dct.keys():
-	#		if type(dct[key]) is unicode:
-	#			v=str(dct[key])
-	#		else:
-	#			v=dct[key]
-	#		d[str(key)]=v
-	#	return d
+def loads(s,object_hook=None):
+	try:
+		return json.loads(s,object_hook=object_hook)
+	except ValueError,e:
+		raise Error(str(e)+" "+s)
 
-	#return json.loads(s, object_hook=object_hook)
-	loads=json.loads
-	load=json.load
-	def dumps(s,default):
-		json.dumps(s,default=default, separators=(',',':'))
-	dump=json.dump
+load=json.load
+def dumps(s,default):
+	json.dumps(s,default=default, separators=(',',':'))
+dump=json.dump
