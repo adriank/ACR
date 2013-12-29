83c3e8e77090d8bb0df6e27a2de82e001c755835
Adrian Kalbarczyk <adrian.kalbarczyk@gmail.com>
2013-12-30 00:44:46 +0100

    Got rid of Mongo's additional JSON types. They are now simple values

--- ACR/serializers/json.py
+++ ACR/serializers/json.py
@@ -2,12 +2,17 @@
 
 from ACR.utils import json_compat
 from pymongo.json_util import default
+from bson.objectid import ObjectId
+
+def hook(dct):
+	r=default(dct)
+	return r.values()[0]
 
 name="JSON"
 def serialize(acenv):
 	if not json_compat:
 		return "ERROR: JSON serializer not installed"
 	#try:
-	return json_compat.dumps(acenv.generations,default=default)
+	return json_compat.dumps(acenv.generations,default=hook)
 	#except:
 	#	raise Exception("simplejson module not installed. Can't output JSON.")
